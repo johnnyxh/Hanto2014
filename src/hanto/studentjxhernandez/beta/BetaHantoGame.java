19a2f03e7579265b45dc4c03feeca4574723d7eb
Johnny <jxhernandez@wpi.edu>
2014-09-13 21:53:57 -0400

    Beta Hanto player color tests

--- src/hanto/studentjxhernandez/beta/BetaHantoGame.java
+++ src/hanto/studentjxhernandez/beta/BetaHantoGame.java
@@ -1,5 +1,8 @@
 package hanto.studentjxhernandez.beta;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import hanto.common.HantoCoordinate;
 import hanto.common.HantoException;
 import hanto.common.HantoGame;
@@ -7,30 +10,61 @@ import hanto.common.HantoPiece;
 import hanto.common.HantoPieceType;
 import hanto.common.HantoPlayerColor;
 import hanto.common.MoveResult;
+import hanto.studentjxhernandez.common.HantoPosition;
+import hanto.studentjxhernandez.common.Piece;
 
 public class BetaHantoGame implements HantoGame {
 	
+	private Map<HantoCoordinate, HantoPiece> board = new HashMap<HantoCoordinate, HantoPiece>();
+	private int numTurns;
+	private HantoPlayerColor movesFirst;
+	private HantoPlayerColor movesSecond;
+	
 	public BetaHantoGame (HantoPlayerColor movesFirst) {
-		
+		numTurns = 0;
+		this.movesFirst = movesFirst;
+		if (movesFirst == HantoPlayerColor.RED) {
+			movesSecond = HantoPlayerColor.BLUE;
+		} else {
+			movesSecond = HantoPlayerColor.RED;
+		}
 	}
 
 	@Override
 	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from,
 			HantoCoordinate to) throws HantoException {
-		// TODO Auto-generated method stub
-		return null;
+		HantoPosition dest = newCords(to.getX(), to.getY());
+		board.put(dest, new Piece(pieceType, getPlayerTurn()));
+		numTurns++;
+		return MoveResult.OK;
 	}
 
 	@Override
 	public HantoPiece getPieceAt(HantoCoordinate where) {
-		// TODO Auto-generated method stub
-		return null;
+		HantoCoordinate dest = newCords(where.getX(), where.getY());
+		return board.get(dest);
 	}
 
 	@Override
 	public String getPrintableBoard() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder printBoard = new StringBuilder();
+		for (Map.Entry<HantoCoordinate, HantoPiece> entry : board.entrySet()) {
+			printBoard
+					.append(entry.getValue() + " AT " + entry.getKey() + "\n");
+		}
+		return printBoard.toString();
+	}
+	
+	private HantoPosition newCords(int x, int y) {
+		return new HantoPosition(x, y);
+	}
+	
+	private HantoPlayerColor getPlayerTurn() {
+		if (numTurns % 2 == 0) {
+			return movesFirst;
+		} else {
+			return movesSecond;
+		}
 	}
 
 }
--- test/BetaHantoGameTest.java
+++ test/BetaHantoGameTest.java
@@ -1,13 +1,34 @@
 import static org.junit.Assert.*;
+import hanto.HantoGameFactory;
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoGame;
+import hanto.common.HantoGameID;
+import hanto.common.HantoPieceType;
+import hanto.common.HantoPlayerColor;
+import hanto.common.MoveResult;
+import hanto.studentjxhernandez.common.HantoPosition;
 
 import org.junit.Test;
-
 
 public class BetaHantoGameTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void betaHantoGameRedFirst() throws HantoException {
+		HantoGame betaGame = HantoGameFactory.getInstance().makeHantoGame(
+				HantoGameID.BETA_HANTO, HantoPlayerColor.RED);
+		HantoCoordinate firstMovePos = new HantoPosition(0, 0);
+		betaGame.makeMove(HantoPieceType.BUTTERFLY, null, firstMovePos);
+		assertEquals(HantoPlayerColor.RED, betaGame.getPieceAt(firstMovePos).getColor());
+	}
+
+	@Test
+	public void betaHantoGameBlueFirst() throws HantoException {
+		HantoGame betaGame = HantoGameFactory.getInstance().makeHantoGame(
+				HantoGameID.BETA_HANTO, HantoPlayerColor.BLUE);
+		HantoCoordinate firstMovePos = new HantoPosition(0, 0);
+		betaGame.makeMove(HantoPieceType.BUTTERFLY, null, firstMovePos);
+		assertEquals(HantoPlayerColor.BLUE, betaGame.getPieceAt(firstMovePos).getColor());
 	}
 
 }
